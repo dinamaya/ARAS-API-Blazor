7f131ee4546650e5a2a2277bcc28f860d63cbdeb
Aldin Amaya <din_amaya@msi-ecs.com.ph>
2025-09-19 11:27:00 +0800

    fix: added MainDbContext

---
+++ ARAS.API/Context/MainDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ARAS.API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ARAS.API.Context
+{
+    public class MainDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public MainDbContext(DbContextOptions<MainDbContext> options) : base(options)
+        {
+        }
+    }
+}
